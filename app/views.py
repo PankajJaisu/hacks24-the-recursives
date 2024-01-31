0e5e86fc241e7ee8079c51c5241c39d2d07b2673
Pankaj Jaiswal <pankajj112223333@gmail.com>
2024-01-31 13:54:00 +0530

    deployment setup

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET'])
+def hello(request):
+    return JsonResponse({"message":"Hello"})
